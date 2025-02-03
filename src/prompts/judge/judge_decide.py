8fc531b7e640a2f69a477ea0fd2bedefc221dcd9
szymon <szymnmazurek@gmail.com>
2025-02-03 21:14:16 +0100

    model evaluation

---
+++ src/prompts/judge/judge_decide.py
@@ -0,0 +1,14 @@
+prompt = """
+    Decide which plot description resembles the one below more and is more accurate to the plot image provided. In order to choose essay you need
+    to answer with word: <FIRST> (indicating the first one is better) or <SECOND> (indicating the second is better).
+
+    Here is the golden standard essay you ought to compare to:
+    {golden_title}
+    {golden_essay}
+
+    <FIRST>
+    {first_essay}
+
+    <SECOND>
+    {second_essay}
+"""
